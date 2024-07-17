058d2b7f52cd7fc9ef78cd68eb436521e81a3514
Kobe Gao <hgao62@uwo.ca>
2024-07-17 15:23:57 -0400

    add data table

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-
 import dash_mantine_components as dmc
-from dash import Dash, dcc, html, Input, Output, dash_table, callback,State
+from dash import Dash, dcc, html, Input, Output, dash_table, callback, State
 import dash_bootstrap_components as dbc
 from db import read_from_sql
-from line_chart import LINE_CHART, generate_stock_line_chart, generate_stock_volume_line_chart
+from line_chart import (
+    LINE_CHART,
+    generate_stock_line_chart_and_underlying_data,
+    generate_stock_volume_line_chart_and_underlying_data,
+)
 from bar_chart import generate_top_three_return_bar_charts
 from pie_chart import generate_sector_pie_chart
 from heading import HEADER
@@ -13,54 +16,72 @@ from typing import List
 from datetime import datetime
 import pandas as pd
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME])
+# The line `import dash_ag_grid as dag` is importing the `dash_ag_grid` library and aliasing it as
+# `dag`. This allows you to refer to the library using the shorter alias `dag` throughout your code
+# instead of typing out the full library name `dash_ag_grid`. This can make your code more concise and
+# easier to read.
+import dash_ag_grid as dag
+
+app = Dash(
+    __name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME]
+)
 
 
 def get_ticker_list():
     df = read_from_sql("select distinct stock from airflow_db.stock_history")
     return list(df["stock"])
 
+
 TICKER_LIST = get_ticker_list()
 
 
 app.layout = html.Div(
-    [    dmc.Title(
-        "Stocks Analytics", align="center"),
-    dmc.Space(h=20),
-    dmc.Button("Download Table Data", id="btn_csv"),
-    dcc.Download(id="download-dataframe-csv"),
-    dmc.Space(h=10),
-    dmc.MultiSelect(
-        label="Select stock you like!",
-        placeholder="Select all stocks you like!",
-        id=DashComponentID.STOCK_DROP_DOWN,
-        value=['AMZN'],
-        data=[{"label": i, "value": i} for i in TICKER_LIST],
-    ),
-    html.Br(),
-    dbc.Row([dbc.Col(dcc.DatePickerRange(
-                id=DashComponentID.DATE_PICKER,
-                start_date=datetime(2024, 1, 1).date(),
-                end_date=datetime(2024, 6,1).date(),
-                min_date_allowed=datetime(2019, 12, 1).date(),
-                max_date_allowed=datetime.today().date(),
-                initial_visible_month=datetime(2024, 1, 1).date(),
-    ),width=2),
-    dbc.Col(dmc.Button(id=DashComponentID.RUN_BTN, children="Run", n_clicks=0, variant="outline"),width=3)
-    ])
-    ,
-    html.Br(),
-    html.Br(),
-     dmc.Container(
     [
-        dmc.Space(h=60),
-        LINE_CHART
-    ],
-    fluid=True,
-)]
+        dmc.Title("Stocks Analytics", align="center"),
+        dmc.Space(h=20),
+        dmc.Button("Download Table Data", id="btn_csv"),
+        dcc.Download(id="download-dataframe-csv"),
+        dmc.Space(h=10),
+        dmc.MultiSelect(
+            label="Select stock you like!",
+            placeholder="Select all stocks you like!",
+            id=DashComponentID.STOCK_DROP_DOWN,
+            value=["AMZN"],
+            data=[{"label": i, "value": i} for i in TICKER_LIST],
+        ),
+        html.Br(),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dcc.DatePickerRange(
+                        id=DashComponentID.DATE_PICKER,
+                        start_date=datetime(2024, 1, 1).date(),
+                        end_date=datetime(2024, 6, 1).date(),
+                        min_date_allowed=datetime(2019, 12, 1).date(),
+                        max_date_allowed=datetime.today().date(),
+                        initial_visible_month=datetime(2024, 1, 1).date(),
+                    ),
+                    width=2,
+                ),
+                dbc.Col(
+                    dmc.Button(
+                        id=DashComponentID.RUN_BTN,
+                        children="Run",
+                        n_clicks=0,
+                        variant="outline",
+                    ),
+                    width=3,
+                ),
+            ]
+        ),
+        html.Br(),
+        html.Br(),
+        dmc.Container(
+            [dmc.Space(h=60), LINE_CHART],
+            fluid=True,
+        ),
+    ]
 )
-
-
 
 
 @callback(
@@ -68,18 +89,42 @@ app.layout = html.Div(
     Output(DashComponentID.VOLUME_CHART, "figure"),
     Output(DashComponentID.CUM_RETURN_CHART, "figure"),
     Output(DashComponentID.SECTOR_PIE_CHART, "figure"),
+    Output(DashComponentID.PRICE_TABLE, "rowData"),
+    Output(DashComponentID.VOLUME_TABLE, "rowData"),
     State(DashComponentID.DATE_PICKER, "start_date"),
     State(DashComponentID.DATE_PICKER, "end_date"),
     State(DashComponentID.STOCK_DROP_DOWN, "value"),
     Input(DashComponentID.RUN_BTN, "n_clicks"),
-  
 )
-def plot_line_chart(start_date, end_date, stocks:List[str],n_clicks):
-    stock_price_line_chart = generate_stock_line_chart(stocks, start_date, end_date,n_clicks)
-    stock_volume_line_chart = generate_stock_volume_line_chart(stocks, start_date, end_date,n_clicks)
-    stock_cum_return_chart = generate_top_three_return_bar_charts(stocks,start_date,end_date, n_clicks)
-    stock_sector_pie_chart = generate_sector_pie_chart(stocks,start_date,end_date,n_clicks)
-    return stock_price_line_chart, stock_volume_line_chart, stock_cum_return_chart,stock_sector_pie_chart
+def plot_line_chart(start_date, end_date, stocks: List[str], n_clicks):
+    (
+        stock_price_line_chart,
+        stock_price_data,
+    ) = generate_stock_line_chart_and_underlying_data(
+        stocks, start_date, end_date, n_clicks
+    )
+    (
+        stock_volume_line_chart,
+        stock_volume_data,
+    ) = generate_stock_volume_line_chart_and_underlying_data(
+        stocks, start_date, end_date, n_clicks
+    )
+    stock_cum_return_chart = generate_top_three_return_bar_charts(
+        stocks, start_date, end_date, n_clicks
+    )
+
+    stock_sector_pie_chart = generate_sector_pie_chart(
+        stocks, start_date, end_date, n_clicks
+    )
+
+    return (
+        stock_price_line_chart,
+        stock_volume_line_chart,
+        stock_cum_return_chart,
+        stock_sector_pie_chart,
+        stock_price_data.to_dict("records"),
+        stock_volume_data.to_dict("records"),
+    )
 
 
 # @callback(
@@ -92,4 +137,4 @@ def plot_line_chart(start_date, end_date, stocks:List[str],n_clicks):
 
 
 if __name__ == "__main__":
-    app.run_server(port = 2409)
+    app.run_server(port=2409)
--- line_chart.py
+++ line_chart.py
@@ -8,11 +8,16 @@ from datetime import datetime
 from utils import generate_stock_list_str, generate_get_stock_price_sql_string
 import pandas as pd
 import plotly.graph_objects as go
+import dash_ag_grid as dag
 
 data = px.data.stocks()
 
 app = Dash(__name__)
 
+STOCK_PRICE_DATA = read_from_sql("SELECT date,close,stock FROM airflow_db.stock_history where stock= 'AMZN' and date >= '2024-01-01'")
+STOCK_VOLUME_DATA = read_from_sql("SELECT date,volume, stock FROM airflow_db.stock_history where stock= 'AMZN' and date >= '2024-01-01'")    
+STOCK_PRICE_TABLE_FIELD = [{"field":"date"},{"field":"close"},{"field":"stock"}]
+STOCK_VOLUME_TABLE_FIELD = [{"field":"date"},{"field":"volume"},{"field":"stock"}]
 
 def convert_to_time_series_format(data:pd.DataFrame) -> pd.DataFrame:
     df = data.pivot_table("close",["date"],"stock")
@@ -24,26 +29,27 @@ def get_stock_price_data(sql_string):
     stock_data_time_series_format = convert_to_time_series_format(stock_data)
     return stock_data_time_series_format
 
-def generate_stock_line_chart(stocks,start_date,end_date, first_load):
+def generate_stock_line_chart_and_underlying_data(stocks,start_date,end_date, first_load):
     if first_load:
      sql_string = generate_get_stock_price_sql_string(stocks, start_date, end_date)
     else:
         sql_string = "SELECT date,close,stock FROM airflow_db.stock_history where stock= 'AMZN' and date >= '2024-01-01'"
         stocks = ["AMZN"]
-    stock_data_time_series_format = get_stock_price_data(sql_string)
-    
+    # stock_data_time_series_format = get_stock_price_data(sql_string)
+    stock_data = read_from_sql(sql_string)
+    stock_data_time_series_format = convert_to_time_series_format(stock_data)
     stock_line_chart = px.line(data_frame=stock_data_time_series_format, x="date", y=stocks, template="seaborn",title="Stock Price Over Time")
     stock_line_chart.update_layout(
         margin=dict(t=50, l=25, r=25, b=25), yaxis_title="Price", xaxis_title="Date",legend_title = "Stock"
     )
-    return stock_line_chart
+    return stock_line_chart,stock_data
 
 def generate_stock_volume_string(stocks,start_date, end_date):
     stock_list_sql_format = generate_stock_list_str(stocks)
     sql_string = f"select date,volume, stock from airflow_db.stock_history where stock in ({stock_list_sql_format}) and date >= '{start_date}' and date<='{end_date}'"
     return sql_string
 
-def generate_stock_volume_line_chart(stocks,start_date,end_date, first_load):
+def generate_stock_volume_line_chart_and_underlying_data(stocks,start_date,end_date, first_load):
     if first_load:
         sql_string = generate_stock_volume_string(stocks, start_date, end_date)
     else:
@@ -58,21 +64,43 @@ def generate_stock_volume_line_chart(stocks,start_date,end_date, first_load):
             facet_col="stock",
             title="Volume Traded Over Time",
           
-        )
+        ), stock_volumes_data
     
 def get_ticker_list():
     df = read_from_sql("select ticker_name from ticker_list")
     return list(df["ticker_name"])
 
-
+STOCK_PRICE_TABLE_COLUMNS = {}
+STOCK_VOLUME_TABLE_COLUMNS = {}
 
 TICKER_LIST = get_ticker_list()
 LINE_CHART =dmc.SimpleGrid(
 children=[
     dcc.Graph(id=DashComponentID.PRICE_CHART),
     dcc.Graph(id=DashComponentID.VOLUME_CHART),
+    dag.AgGrid(
+            id=DashComponentID.PRICE_TABLE,
+            columnSize="sizeToFit",
+            columnDefs=STOCK_PRICE_TABLE_FIELD,
+            rowData=STOCK_PRICE_DATA.to_dict("records"),
+            csvExportParams={
+                "fileName": "price_table.csv",
+            },
+            dashGridOptions={'pagination':True,"grid-page-size":10},
+        ),
+     dag.AgGrid(
+            id=DashComponentID.VOLUME_TABLE,
+            columnSize="sizeToFit",
+            columnDefs=STOCK_VOLUME_TABLE_FIELD,
+            rowData=STOCK_PRICE_DATA.to_dict("records"),
+            csvExportParams={
+                "fileName": "volume_table.csv",
+            },
+            dashGridOptions={'pagination':True,"grid-page-size":10},
+        ),
     dcc.Graph(id=DashComponentID.CUM_RETURN_CHART),
     dcc.Graph(id=DashComponentID.SECTOR_PIE_CHART)
+    
     # dash_table.DataTable(
     #     data.to_dict("records"),
     #     [{"name": i, "id": i} for i in data.columns],
@@ -96,3 +124,5 @@ breakpoints=[
     },  # common screen size for tablets
 ],
 )
+# ,id = DashComponentID.ALL_CHARTS
+# )
